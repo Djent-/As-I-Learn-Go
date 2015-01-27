cf6e7754006db61c3d8889e844251c67c395c763
Djent- <inflames2361@aim.com>
2015-01-26 19:54:49 -0500

    Added first exercise

---
+++ hello.go
@@ -0,0 +1,10 @@
+// This work is copyright Mark McGranaghan and licensed under a Creative Commons Attribution 3.0 Unported License.
+// http://creativecommons.org/licenses/by/3.0/
+
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("hello world")
+}
