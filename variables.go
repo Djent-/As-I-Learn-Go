f048484f169e1a6530b6a72a62c623df1374cffd
Djent- <inflames2361@aim.com>
2015-01-26 20:24:36 -0500

    Added third exercise

---
+++ variables.go
@@ -0,0 +1,24 @@
+// This work is copyright Mark McGranaghan and licensed under a Creative Commons Attribution 3.0 Unported License.
+// http://creativecommons.org/licenses/by/3.0/
+
+package main
+
+import "fmt"
+
+func main(){
+	
+	var a string = "initial"
+	fmt.Println(a)
+	
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+	
+	var d = true
+	fmt.Println(d)
+	
+	var e int
+	fmt.Println(e)
+	
+	f := "short"
+	fmt.Println(f)
+}
