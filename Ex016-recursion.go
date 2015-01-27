bea3baea52cb84ecaa3b1fd00fbcf424f1b5ec53
Djent- <inflames2361@aim.com>
2015-01-27 00:12:25 -0500

    Added sixteenth exercise

---
+++ Ex016-recursion.go
@@ -0,0 +1,17 @@
+// This work is copyright Mark McGranaghan and licensed under a Creative Commons Attribution 3.0 Unported License.
+// http://creativecommons.org/licenses/by/3.0/
+
+package main
+
+import "fmt"
+
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
+func main() {
+	fmt.Println(fact(7))
+}
