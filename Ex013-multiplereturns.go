98694501199b786056f5320ebc502fe1960311cf
Djent- <inflames2361@aim.com>
2015-01-27 00:02:33 -0500

    Added thirteenth exercise

---
+++ Ex013-multiplereturns.go
@@ -0,0 +1,19 @@
+// This work is copyright Mark McGranaghan and licensed under a Creative Commons Attribution 3.0 Unported License.
+// http://creativecommons.org/licenses/by/3.0/
+
+package main
+
+import "fmt"
+
+func vals() (int, int) {
+	return 3, 7
+}
+
+func main() {
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
+	
+	_, c := vals()
+	fmt.Println(c)
+}
