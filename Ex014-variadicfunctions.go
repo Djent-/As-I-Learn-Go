1aa3dcd0ec856833ffe2ac438c341e824101783d
Djent- <inflames2361@aim.com>
2015-01-27 00:05:36 -0500

    Added fourteenth exercise

---
+++ Ex014-variadicfunctions.go
@@ -0,0 +1,23 @@
+// This work is copyright Mark McGranaghan and licensed under a Creative Commons Attribution 3.0 Unported License.
+// http://creativecommons.org/licenses/by/3.0/
+
+package main
+
+import "fmt"
+
+func sum(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
+
+func main() {
+	sum(1, 2)
+	sum(1, 2, 3)
+	
+	nums := []int{1, 2, 3, 4}
+	sum(nums...)
+}
