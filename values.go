d9ba23f95dffae5470a6efd28754fa75b44592f5
Djent- <inflames2361@aim.com>
2015-01-26 20:22:13 -0500

    Added second exercise

---
+++ values.go
@@ -0,0 +1,17 @@
+// This work is copyright Mark McGranaghan and licensed under a Creative Commons Attribution 3.0 Unported License.
+// http://creativecommons.org/licenses/by/3.0/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+	
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 = ", 7.0/3.0)
+	
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
