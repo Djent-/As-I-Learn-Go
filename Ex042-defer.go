4667f21c2c5e415c2ba9c119161fa23b07208afe
Djent- <inflames2361@aim.com>
2015-01-27 18:46:10 -0500

    Added fortysecond example

---
+++ Ex042-defer.go
@@ -0,0 +1,32 @@
+// This work is copyright Mark McGranaghan and licensed under a Creative Commons Attribution 3.0 Unported License.
+// http://creativecommons.org/licenses/by/3.0/
+
+package main
+
+import "fmt"
+import "os"
+
+func main() {
+	f := createFile("/tmp/defer.txt")
+	defer closeFile(f)
+	writeFile(f)
+}
+
+func createFile(p string) *os.File {
+	fmt.Println("creating")
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func writeFile(f *os.File) {
+	fmt.Println("writing")
+	fmt.Fprintln(f, "data")
+}
+
+func closeFile(f *os.File) {
+	fmt.Println("closing")
+	f.Close()
+}
