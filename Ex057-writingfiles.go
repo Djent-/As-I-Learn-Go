008bdfb0e8e031434f7ac5dee6afa9ce2a198ffc
Djent- <inflames2361@aim.com>
2015-01-28 12:52:46 -0500

    Added fiftyseventh exercise

---
+++ Ex057-writingfiles.go
@@ -0,0 +1,18 @@
+// This work is copyright Mark McGranaghan and licensed under a Creative Commons Attribution 3.0 Unported License.
+// http://creativecommons.org/licenses/by/3.0/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic (e)
+	}
+}
+
