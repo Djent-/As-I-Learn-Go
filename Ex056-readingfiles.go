93683d53d98faa933e6580c105f21648f27199f5
Djent- <inflames2361@aim.com>
2015-01-28 12:26:11 -0500

    Added fiftysixth exercise

---
+++ Ex056-readingfiles.go
@@ -0,0 +1,23 @@
+// This work is copyright Mark McGranaghan and licensed under a Creative Commons Attribution 3.0 Unported License.
+// http://creativecommons.org/licenses/by/3.0/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+//error helper
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	
+}
