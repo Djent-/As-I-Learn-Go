c92f4b5ca75bf6349adc62c5e5ffacc95cffcaf9
Djent- <inflames2361@aim.com>
2015-01-27 19:24:36 -0500

    Added fortyfourth example

---
+++ Ex044-stringfunctions.go
@@ -0,0 +1,30 @@
+// This work is copyright Mark McGranaghan and licensed under a Creative Commons Attribution 3.0 Unported License.
+// http://creativecommons.org/licenses/by/3.0/
+
+package main
+
+import s "strings"
+import "fmt"
+
+//alias
+var p = fmt.Println
+
+func main() {
+	p("constains:", s.Contains("test", "es"))
+	p("Count:", s.Count("test", "t"))
+	p("HasPrefix:", s.HasPrefix("test", "te"))
+	p("HasSuffix:", s.HasSuffix("test", "st"))
+	p("Index:", s.Index("test", "e"))
+	p("Join:", s.Join([]string{"a", "b"}, "-"))
+	p("Repeat:", s.Repeat("a", 5))
+	p("Replace:", s.Replace("foo", "o", "0", -1))
+	p("Replace:", s.Replace("foo", "o", "0", 1))
+	p("Split:", s.Split("a-b-c-d-e", "-"))
+	p("ToLower:", s.ToLower("TEST"))
+	p("ToUpper:", s.ToUpper("test"))
+	p()
+	
+	p("Len:", len("ehllo"))
+	p("Char:", "hello"[1])
+	
+}
