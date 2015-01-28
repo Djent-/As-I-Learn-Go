cde74be23b296a2e9c32712ca4560a6e49baf35f
Djent- <inflames2361@aim.com>
2015-01-28 13:26:04 -0500

    Finished fiftyseventh exercise

--- Ex057-writingfiles.go
+++ Ex057-writingfiles.go
@@ -16,3 +16,32 @@ func check(e error) {
 	}
 }
 
+func main() {
+	d1 := []byte("hello\ngo\n")
+	err := ioutil.WriteFile("test.txt", d1, 0644)
+	check(err)
+	
+	//granular writes
+	f, err := os.Create("test1.txt")
+	check(err)
+	
+	defer f.Close()
+	
+	d2 := []byte{115, 111, 109, 101, 10}
+	n2, err := f.Write(d2)
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n2)
+	
+	n3, err := f.WriteString("writes\n")
+	fmt.Printf("wrote %d bytes\n", n3)
+	
+	//issue a Sync to flush writes to stable storage
+	f.Sync()
+	
+	w := bufio.NewWriter(f)
+	n4, err := w.WriteString("buffered\n")
+	fmt.Printf("wrote %d bytes\n", n4)
+	
+	//use Flush to ensure all buffered operations have been applied
+	w.Flush()
+}
