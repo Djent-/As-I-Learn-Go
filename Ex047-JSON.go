c2363a7ed721d8f5de3f8aa4e63f7ddd7d81d851
Djent- <inflames2361@aim.com>
2015-01-28 00:38:17 -0500

    Working on fortyseventh exercise

--- Ex047-JSON.go
+++ Ex047-JSON.go
@@ -18,3 +18,22 @@ type Response2 struct {
 	Page int `json:"page"`
 	Fruits []string `json:"fruits"`
 }
+
+func main() {
+	//encoding basic types to JSON strings
+	//here are some examples for atomic values
+	bolB, _ := json.Marshal(true)
+	fmt.Println(string(bolB))
+	
+	intB, _ := json.Marshal(1)
+	fmt.Println(string(intB))
+	
+	fltB, _ := json.Marshal(2.34)
+	fmt.Println(string(fltB))
+	
+	strB, _ := json.Marshal("gopher")
+	fmt.Println(string(strB))
+	
+	//here are some for slices and maps which encode to json arrays
+	
+}
