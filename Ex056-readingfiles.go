f3d9df3e833f20f039157c55d84fb35d7e1f0469
Djent- <inflames2361@aim.com>
2015-01-28 12:32:07 -0500

    Working on fiftysixth exercise

--- Ex056-readingfiles.go
+++ Ex056-readingfiles.go
@@ -19,5 +19,38 @@ func check(e error) {
 }
 
 func main() {
+	dat, err := ioutil.ReadFile("/tmp/dat")
+	check(err)
+	fmt.Print(string(dat))
+	
+	f, err := os.Open("/tmp/dat")
+	check(err)
+	
+	b1 := make([]byte, 5)
+	n1, err := f.Read(b1)
+	check(err)
+	fmt.Printf("%d bytes: %s\n", n1m string(b1))
+	
+	//seek to a known location in a file and Read from there
+	os, err := f.Seek(6, 0)
+	check(err)
+	b2 := make([]byte, 2)
+	n2, err := f.Read(b2)
+	check(err)
+	fmt.Printf("%d bytes @ %d: %s\n", n2, o2, string(b2))
+	
+	//reads like the ones above can be more robustly implemented with
+	//ReadAtLeast
+	o3, err := f.Seek(6, 0)
+	check(err)
+	b3 := make([]byte, 2)
+	n3, err := io.ReadAtLeast(f, b3, 2)
+	check(err)
+	fmt.Printf("%d bytes @ %d: %s\n", n3, o3, string(b3))
+	
+	//there's no built in rewind, but Seek(0, 0) accomplishes this
+	_, err = f.Seek(0, 0)
+	check(err)
+	
 	
 }
